49fead89de524729719c1baea569edda447cefb2
Elizbar_kheladze <kheladzee2001@gmail.com>
2026-01-22 10:56:58 +0100

    Initial Commit

---
+++ Quies Watch App/QuiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuiesApp.swift
+//  Quies Watch App
+//
+//  Created by Elizbar Kheladze on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Quies_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
